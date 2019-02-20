34f18051ce097959f72e3b0eb4467bb0f89e4ffa
Konrad Kusiak <konrad_kusiak@epam.com>
2019-02-20 08:10:00 +0100

    rozszerzono TesterStatistics o metody udane i nie wymaga refaktoru aby nie klasa nie drukowała wyników

--- Tester/src/main/java/pl/kk/tester/Tester.java
+++ Tester/src/main/java/pl/kk/tester/Tester.java
@@ -7,7 +7,11 @@ import java.lang.reflect.Method;
 
 public class Tester {
 
-    private static final TesterStatistics statistics = new TesterStatistics();
+    private final TesterStatistics statistics;
+
+    public Tester(TesterStatistics statistics) {
+        this.statistics = statistics;
+    }
 
     public void process(Class<?> testedClass, Class<? extends Annotation> annotation) throws NoSuchMethodException, IllegalAccessException, InvocationTargetException, InstantiationException {
 
@@ -18,17 +22,19 @@ public class Tester {
             try {
                 if (method.isAnnotationPresent(annotation)) {
                     method.invoke(testObject);
-                    statistics.addSuccessfulRun();
+                    statistics.logSuccessfulRun(method);
                 }
             } catch (IllegalArgumentException e) {
+                statistics.logFailedRun(method, e.getMessage());
                 System.err.format("metoda %s - nieodpowiednia ilość argumentów (%s) %n", method, e.getMessage());
             } catch (IllegalAccessException e) {
+                statistics.logFailedRun(method, e.getMessage());
                 System.err.format("metoda %s - dostęp zabroniony? %s przyczyna: %s %n", method, e.getMessage(), e.getCause());
             } catch (InvocationTargetException e) {
+                statistics.logFailedRun(method, e.getMessage());
                 System.err.format("metoda %s - wywołanie nieudane z racji na %s przyczyna: %s %n", method, e.getMessage(), e.getCause());
             }
         }
-        System.out.format("Odpalone metody : %d%n", statistics.methodsRan());
     }
 }
 
--- Tester/src/main/java/pl/kk/tester/TesterStatistics.java
+++ Tester/src/main/java/pl/kk/tester/TesterStatistics.java
@@ -1,14 +1,46 @@
 package pl.kk.tester;
 
-class TesterStatistics {
+import java.io.PrintStream;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
-    private int successCount = 0;
+public class TesterStatistics {
 
-    void addSuccessfulRun() {
-        successCount++;
+    private final List<Method> methodsSucceed = new ArrayList<>();
+    private final Map<Method, String> methodsFailed = new HashMap<>();
+    private int countSucceed;
+    private int countFailed;
+
+
+    void logSuccessfulRun(Method method) {
+        methodsSucceed.add(method);
+        countSucceed++;
     }
 
-    int methodsRan() {
-        return successCount;
+    void logFailedRun(Method method, String message) {
+        methodsFailed.putIfAbsent(method, message);
+        countFailed++;
+    }
+
+    public void printStatistics(PrintStream printStream) {
+        printSucceed(printStream);
+        printFailed(printStream);
+    }
+
+    private void printSucceed(PrintStream printStream) {
+        for (Method method : methodsSucceed) {
+            printStream.printf("Method succeed: %s%n", method);
+        }
+        printStream.printf("All methods succeed: %d%n", countSucceed);
+    }
+
+    private void printFailed(PrintStream printStream) {
+        for (Map.Entry<Method,String> entry : methodsFailed.entrySet()) {
+            printStream.printf("Method failed: %s; cause: %s%n", entry.getKey().toString(), entry.getValue());
+        }
+        printStream.printf("All methods failed: %d%n", countFailed);
     }
 }
